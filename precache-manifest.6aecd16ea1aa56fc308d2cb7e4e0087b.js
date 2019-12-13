918792d89c09b45e8157e68a8a442fea11fbfda2
Bohdan <bohdanimiela@gmail.com>
2019-12-13 17:18:17 +0100

    Updates

---
+++ precache-manifest.6aecd16ea1aa56fc308d2cb7e4e0087b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957a32b98e57bc3022f423188618f7c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d964df9c08b2ddc1f870",
+    "url": "/static/css/main.ca5365c5.chunk.css"
+  },
+  {
+    "revision": "424e2ea20cb7b6137caa",
+    "url": "/static/js/2.db0320df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.db0320df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d964df9c08b2ddc1f870",
+    "url": "/static/js/main.48ef4294.chunk.js"
+  },
+  {
+    "revision": "3542c767df2e67fb7f1e",
+    "url": "/static/js/runtime-main.b04fd9d3.js"
+  },
+  {
+    "revision": "dfa4e760a50250ea966909ce0a1bf95a",
+    "url": "/static/media/Zouk.dfa4e760.mp3"
+  },
+  {
+    "revision": "dec7c367984f3d6b38f9a74f6c1ecb88",
+    "url": "/static/media/drummer.dec7c367.png"
+  },
+  {
+    "revision": "39408019c3fac35907df6a6f2542d3b3",
+    "url": "/static/media/drummer@2x.39408019.png"
+  },
+  {
+    "revision": "6ce68fff8ddf22aee137af8eb3489d0a",
+    "url": "/static/media/drummer@3x.6ce68fff.png"
+  },
+  {
+    "revision": "20e4fd7eed6831c1778aa056a32b3244",
+    "url": "/static/media/reem-kufi-latin-400.20e4fd7e.woff"
+  },
+  {
+    "revision": "983778912ae31484a6e9ea6f36cae1b9",
+    "url": "/static/media/reem-kufi-latin-400.98377891.woff2"
+  }
+]);
