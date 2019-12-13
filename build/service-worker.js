e55cb5dc6d878deaf2eef588dcbbdbb1c298454f
Bohdan <bohdanimiela@gmail.com>
2019-12-13 17:41:50 +0100

    Adjust vh

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.aeb9edc1eeef7b21b6868e38f8464129.js"
+  "/precache-manifest.6aecd16ea1aa56fc308d2cb7e4e0087b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './components/App';
 
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * .01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
