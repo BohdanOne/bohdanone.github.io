b6e08644907b31b83504b0dbdf1a29515b3ca488
Bohdan <bohdanimiela@gmail.com>
2019-12-26 17:26:59 +0100

    Add MainNav, change scss vars to css vars

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import MainNav from '../parts/MainNav';
 import SoundToggler from '../parts/SoundToggler';
 // import Hamburger from '../parts/Hamburger';
 
 export default () => {
   return (
     <header className="Header">
-      <SoundToggler />
+      <MainNav />
       {/* <Hamburger /> */}
+      <SoundToggler />
     </header>
   );
 };
---
+++ src/components/parts/MainNav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <nav>
+      <button className="nav-button" aria-pressed="true">
+        about
+      </button>
+      {/* <button className="nav-button" aria-pressed="false">
+        skills
+      </button>
+      <button className="nav-button" aria-pressed="false">
+        projects
+      </button>
+      <button className="nav-button" aria-pressed="false">
+        contact
+      </button> */}
+    </nav>
+  );
+}
