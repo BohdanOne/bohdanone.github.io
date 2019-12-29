633494a27e6f631fac8d27e0b1a75c2a09d191a0
Bohdan <bohdanimiela@gmail.com>
2019-12-29 11:51:59 +0100

    Refactor AudioTogglingImage

--- src/components/AboutPage/AudioTogglingImage.js
+++ src/components/AboutPage/AudioTogglingImage.js
@@ -1,72 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import noSoundNotification from './audio-player/noSoundNotification';
 import audioPlayer from './audio-player/audioPlayer';
 import audioSource from '../../assets/sounds/Zouk.mp3';
 
+const AudioTogglingImage = props => {
+  const [audio, setAudio] = useState();
+  const [ctx, setCtx] = useState();
+  const [isPlaying, setIsPlaying] = useState(false);
 
-class AudioTogglingImage extends React.Component {
-  state = {
-    audio: null,
-    ctx: null,
-    isPlaying: false
-  };
-
-  componentDidMount() {
-    if(this.props.sound) {
-      const [audio, ctx] = audioPlayer(audioSource);
-      this.setState({ audio, ctx });
-    }
-  };
-
-  componentDidUpdate() {
-    if (this.props.sound && this.state.isPlaying) {
-      if (this.state.audio) {
-        this.state.ctx.resume();
-        this.state.audio.play();
-      } else {
+  useEffect(() => {
+    if (props.sound) {
+      if (!audio) {
         const [audio, ctx] = audioPlayer(audioSource);
-        this.setState({ audio, ctx });
+        setAudio(audio);
+        setCtx(ctx);
+      }
+      if (isPlaying) {
+        ctx.resume();
+        audio.play();
+      } else {
+        audio && audio.pause();
       }
     } else {
-      this.state.audio && this.state.audio.pause();
+      audio && audio.pause();
     }
-  }
+    return () => audio && audio.pause();
+  }, [props.sound, isPlaying, audio, ctx]);
 
-  async componentWillUnmount() {
-    if(this.state.ctx) {
-      await this.state.ctx.suspend();
-    }
-  }
-
-  playSound = () => this.setState({ isPlaying: ! this.state.isPlaying });
-
-  onImageInteraction = sound => {
-    !sound ?
-      noSoundNotification() :
-      this.playSound();
-  }
-
-  render() {
-    return (
-      <>
-        <img
-          className={ this.props.imgClass }
-          src={ this.props.imgSrc[0] }
-          srcSet={ `${this.props.imgSrc[0]}, ${this.props.imgSrc[1]} 2x, ${this.props.imgSrc[2]} 3x` }
-          alt={ this.props.imgAlt }
-          onClick={ () => this.onImageInteraction(this.props.sound) }
-          onKeyPress={ () => this.onImageInteraction(this.props.sound) }
-          tabIndex="0"
-          role="button"
-        />
-      </>
-    );
+  const onImageInteraction = sound => {
+    !sound ? noSoundNotification() : setIsPlaying(!isPlaying);
   };
-};
 
-const mapStateToProps = state => {
-  return { sound: state.sound.isOn };
+  return (
+    <>
+      <img
+        className={ props.imgClass }
+        src={ props.imgSrc[0] }
+        srcSet={ `${props.imgSrc[0]}, ${props.imgSrc[1]} 2x, ${props.imgSrc[2]} 3x` }
+        alt={ props.imgAlt }
+        onClick={ () => onImageInteraction(props.sound) }
+        onKeyPress={ () => onImageInteraction(props.sound) }
+        tabIndex="0"
+        role="button"
+      />
+    </>
+  );
 }
+
+const mapStateToProps = state => { return { sound: state.sound.isOn } };
 
 export default connect(mapStateToProps)(AudioTogglingImage);
