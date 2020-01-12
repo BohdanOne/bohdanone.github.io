5b5ce5c60d81006d9fcfe450bce21cb7e4cc3d88
Bohdan <bohdanimiela@gmail.com>
2020-01-12 10:40:55 +0100

    Fix style issues

--- src/components/Header/Hamburger.js
+++ src/components/Header/Hamburger.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <button >
-      <svg className="icon hamburger" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4 7V9H28V7H4ZM4 15V17H28V15H4ZM4 23V25H28V23H4Z"/>
-      </svg>
-    </button>
-  );
+export default props => {
+  const renderBurger = () => {
+    if (props.open) {
+      return (
+        <button
+          onClick={ props.toggleBurger }
+          role="switch" aria-checked="true"
+          className="icon hamburger"
+        >
+          <svg
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M4 7V9H28V7H4ZM4 23V25H28V23H4Z"/>
+          </svg>
+        </button>
+      );
+    } else {
+      return (
+        <button
+          onClick={ props.toggleBurger }
+          role="switch" aria-checked="false"
+          className="icon hamburger"
+        >
+          <svg
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M4 7V9H28V7H4ZM4 15V17H28V15H4ZM4 23V25H28V23H4Z"/>
+          </svg>
+        </button>
+      );
+    }
+  }
+  return renderBurger();
 };
