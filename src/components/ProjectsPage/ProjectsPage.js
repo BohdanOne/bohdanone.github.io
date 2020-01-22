a1d863b30d49ea7c24a68c02db81b22f46f00a55
Bohdan <bohdanimiela@gmail.com>
2020-01-22 12:49:26 +0100

    Add more projects

--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTrail, animated } from 'react-spring';
-import projects from './projects';
+import projectsList from './projects';
 
 const config = { mass: 5, tension: 2000, friction: 350 };
 
 export default () => {
+  const [searchTerm, setSearch] = useState('');
+  let projects = !searchTerm ? projectsList : projectsList.filter(project => project.technologies.includes(searchTerm));
+
   const trail = useTrail(projects.length, {
     config,
     opacity: 1,
@@ -17,6 +20,27 @@ export default () => {
   return (
   <section className="ProjectsPage">
     <h2 className="ProjectsPage__heading">Projects</h2>
+    <div className="ProjectsPage__filter">
+      <label>Filter by technology:
+        <select defaultValue={searchTerm} onChange={e => setSearch(e.target.value)}>
+          <option value="HTML5" >HTML5</option>
+          <option value="CSS3">CSS3</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="Node.js">Node.js</option>
+          <option value="Express.js">Express.js</option>
+          <option value="EJS">EJS</option>
+          <option value="MongoDB">MongoDB</option>
+          <option value="Bootstrap">Bootstrap</option>
+          <option value="SCSS">SCSS</option>
+          <option value="React">React</option>
+          <option value="Redux">Redux</option>
+          <option value="Svelte">Svelte</option>
+          <option value="Sapper">Sapper</option>
+          <option value="Socket.IO">Socket.IO</option>
+          <option value="Webpack">Webpack</option>
+        </select>
+      </label>
+    </div>
     <div className="ProjectsPage__container">
       { trail.map(({x, height, ...rest}, index) => (
         <animated.div
@@ -35,28 +59,18 @@ export default () => {
           />
           <p className="project__description">
             { projects[index].description }
-            <br/>
-            <span>Technologies: </span>{ projects[index].technologies }
           </p>
-          <p className="project__links">
+          <p className="project__technologies">
+            <span>Built with: </span>{ projects[index].technologies.join(', ') }.
+          </p>
+          <div className="project__links">
             <a className="project__link" href={ projects[index].demoUrl}>DEMO</a>
             <a className="project__link" href={ projects[index].codeUrl}>CODE</a>
-          </p>
+          </div>
           <hr/>
         </animated.div>
       ))}
     </div>
-    {/* <div className="temporary-info">
-      <h3>Site under development</h3>
-      <p>Come back soon.<br/>
-        In the meantime you can visit my GitHub profile
-      </p>
-      <a href="https://github.com/BohdanOne" className="contact-icon github" alt="github link">
-        <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-          <path d="M32 8C18.7422 8 8 18.7422 8 32C8 42.6016 14.875 51.6016 24.4141 54.7734C25.6172 54.9922 26.0547 54.2578 26.0547 53.6172C26.0547 53.0469 26.0312 51.5391 26.0234 49.5391C19.3437 50.9844 17.9375 46.3203 17.9375 46.3203C16.8438 43.5469 15.2734 42.8047 15.2734 42.8047C13.0938 41.3203 15.4375 41.3516 15.4375 41.3516C17.8438 41.5234 19.1094 43.8203 19.1094 43.8203C21.25 47.4922 24.7266 46.4297 26.0938 45.8203C26.3125 44.2656 26.9375 43.2109 27.6172 42.6094C22.2891 42.0078 16.6875 39.9453 16.6875 30.75C16.6875 28.125 17.625 25.9844 19.1562 24.3047C18.9141 23.7031 18.0859 21.2578 19.3906 17.9531C19.3906 17.9531 21.4062 17.3125 25.9922 20.4141C27.9062 19.8828 29.9609 19.6172 32 19.6094C34.0391 19.6172 36.0938 19.8828 38.0078 20.4141C42.5938 17.3125 44.6016 17.9531 44.6016 17.9531C45.9141 21.2578 45.0938 23.7031 44.8438 24.3047C46.3828 25.9844 47.3047 28.125 47.3047 30.75C47.3047 39.9688 41.6953 41.9922 36.3516 42.5938C37.2109 43.3281 37.9766 44.7969 37.9766 47.0312C37.9766 50.2422 37.9531 52.8281 37.9531 53.6172C37.9531 54.2578 38.3828 55.0078 39.6016 54.7734C49.1328 51.5937 56 42.6016 56 32C56 18.7422 45.2578 8 32 8Z" />
-        </svg>
-      </a>
-    </div> */}
   </section>
   );
 };
--- src/components/ProjectsPage/projects.js
+++ src/components/ProjectsPage/projects.js
@@ -4,23 +4,56 @@ import vinylL from '../../assets/images/vinyl-store-800.png';
 import gibberS from '../../assets/images/gibber-200.png';
 import gibberM from '../../assets/images/gibber-400.png';
 import gibberL from '../../assets/images/gibber-800.png';
+import tempomatS from '../../assets/images/tempomat-200.png';
+import tempomatM from '../../assets/images/tempomat-400.png';
+import tempomatL from '../../assets/images/tempomat-800.png';
+import simple808S from '../../assets/images/simple808-200.png';
+import simple808M from '../../assets/images/simple808-400.png';
+import simple808L from '../../assets/images/simple808-800.png';
+import weatherS from '../../assets/images/weather-200.png';
+import weatherM from '../../assets/images/weather-400.png';
+import weatherL from '../../assets/images/weather-800.png';
 
 const projects = [
   {
     name: 'Vinyl Store',
-    description: 'Application wich lets you browse, add and comment your favorite record stores.',
+    description: 'Application for all music lovers. Guest user can browse collection of record stores (each entry has info, rating, location on google maps, opening hours and reviews). Registered user can edit or remove own profile; see other users profiles; add, edit and delete stores; add, edit and delete reviews. Demo hosted on Heroku - might take few seconds to load!',
     imgs: [ vinylS, vinylM, vinylL ],
-    technologies: 'MongoDB, Express.js, Node.js, EJS, Bootstrap',
+    technologies: ['MongoDB', 'Express.js', 'Node.js', 'EJS', 'Bootstrap', 'JavaScript', 'CSS3', 'HTML5'],
     demoUrl: 'https://vinyl-store.herokuapp.com/',
     codeUrl: 'https://github.com/BohdanOne/vinyl-store'
   },
   {
     name: '! gibber',
-    description: 'Minimal Chat App.',
+    description: 'Minimal Chat App. My first experiments with Sapper and with Socket.IO. Demo hosted on Heroku - might take few seconds to load. App still under development - more features on the way!',
     imgs: [ gibberS, gibberM, gibberL ],
-    technologies: 'Svelte, Sapper, Socket.IO',
-    demoUrl: 'https://giber-chat.herokuapp.com/',
+    technologies: ['Svelte', 'Sapper', 'Socket.IO', 'JavaScript', 'CSS3', 'HTML5'],
+    demoUrl: 'https://gibber-chat.herokuapp.com/',
     codeUrl: 'https://github.com/BohdanOne/gibber'
+  },
+  {
+    name: 'Tempomat',
+    description: 'Tool for musicians. Convert BPM (beats-per-minute) to Millisceconds. App also display italian tempo markings name for given BPM. By default it uses quarter note subdivision, but user can also choose other subdivisions. This is my first application written in Svelte.',
+    imgs: [ tempomatS, tempomatM, tempomatL ],
+    technologies: ['Svelte', 'JavaScript', 'CSS3', 'HTML5'],
+    demoUrl: 'https://bohdanone.github.io/tempomat',
+    codeUrl: 'https://github.com/BohdanOne/tempomat'
+  },
+  {
+    name: 'Simple 808',
+    description: 'Drum machine with loaded sound of legendary TR-808. You can play using keyboard or pressing pads on the screen',
+    imgs: [ simple808S, simple808M, simple808L ],
+    technologies: ['JavaScript', 'CSS3', 'HTML5'],
+    demoUrl: 'https://bohdanone.github.io/simple808',
+    codeUrl: 'https://github.com/BohdanOne/simple808'
+  },
+  {
+    name: "What's the Weather",
+    description: 'User can check actual weather condition or forecasts fetched from AccuWeather API. If user allow geolocation then app will determine his location using browsers Geolocation API. If geolocation is not allowed (or user wants to check weather for other place) there is also text search option.',
+    imgs: [ weatherS, weatherM, weatherL ],
+    technologies: ['JavaScript', 'CSS3', 'HTML5', 'SCSS', 'Webpack'],
+    demoUrl: 'https://bohdanone.github.io/whatstheweather',
+    codeUrl: 'https://github.com/BohdanOne/whatstheweather'
   }
 ];
 
