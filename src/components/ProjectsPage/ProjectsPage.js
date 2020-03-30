77a2c2f701fbb96ee89aee631accefdeb11b3070
Bohdan <bohdanimiela@gmail.com>
2020-03-30 20:33:47 +0200

    Implement image optimization

--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -7,74 +7,89 @@ import animationConfig from '../utils/animationConfig';
 export default () => {
   const [searchTerm, setSearch] = useState('');
 
-  let projects = !searchTerm ? projectsList : projectsList.filter(
-    project => project.technologies.includes(searchTerm)
-  );
+  let projects = !searchTerm
+    ? projectsList
+    : projectsList.filter(project => project.technologies.includes(searchTerm));
 
   const trail = useTrail(projects.length, animationConfig);
 
   return (
-  <section className="projects-page">
-    <animated.h2 className="title" style={useSpring(animationConfig)}>Projects</animated.h2>
-    <animated.div className="filter" style={useSpring(animationConfig)}>
-      <label className="filter__label">Filter by technology:
-        <select
-          className="filter__select"
-          defaultValue={searchTerm}
-          onChange={e => setSearch(e.target.value)}
-        >
-          <option value="HTML5">HTML5</option>
-          <option value="CSS3">CSS3</option>
-          <option value="JavaScript">JavaScript</option>
-          <option value="Node.js">Node.js</option>
-          <option value="Express.js">Express.js</option>
-          <option value="EJS">EJS</option>
-          <option value="MongoDB">MongoDB</option>
-          <option value="Bootstrap">Bootstrap</option>
-          <option value="SASS">SASS</option>
-          <option value="React">React</option>
-          <option value="Redux">Redux</option>
-          <option value="Svelte">Svelte</option>
-          <option value="Sapper">Sapper</option>
-          <option value="Socket.IO">Socket.IO</option>
-          <option value="Webpack">Webpack</option>
-          <option value="TypeScript">TypeScript</option>
-        </select>
-      </label>
-    </animated.div>
-    <div className="projects-container">
-      { trail.map(({x, height, ...rest}, index) => (
-        <animated.div
-          className="project"
-          key={ projects[index].name }
-          style={{ ...rest, transform: x.interpolate(x => `translate3d(0, ${x}px, 0)`)}}
-        >
-          <h3 className="project__title">
-            { projects[index].name }
-          </h3>
-          <img
-            className="project__img"
-            src={projects[index].imgs[0]}
-            srcSet={`${projects[index].imgs[0]} 200w, ${projects[index].imgs[1]} 400w, ${projects[index].imgs[2]} 800w`}
-            sizes="(max-width: 300px) 200px, (max-width: 400px) 400px, 800px"
-            alt={`${projects[index].name} screenshot`}
-          />
-          <p className="project__description">
-            { projects[index].description }
-          </p>
-          <p className="project__technologies">
-            <span>Built with: </span>{ projects[index].technologies.join(', ') }.
-          </p>
-          <div className="project__links">
-            <a className="project__link" href={ projects[index].demoUrl}>DEMO</a>
-            <a className="project__link" href={ projects[index].codeUrl}>CODE</a>
-          </div>
-          <nav className="project__nav">
-            <BackToTopButton />
-          </nav>
-        </animated.div>
-      ))}
-    </div>
-  </section>
+    <section className='projects-page'>
+      <animated.h2 className='title' style={useSpring(animationConfig)}>
+        Projects
+      </animated.h2>
+      <animated.div className='filter' style={useSpring(animationConfig)}>
+        <label className='filter__label'>
+          Filter by technology:
+          <select
+            className='filter__select'
+            defaultValue={searchTerm}
+            onChange={e => setSearch(e.target.value)}
+          >
+            <option value='HTML5'>HTML5</option>
+            <option value='CSS3'>CSS3</option>
+            <option value='JavaScript'>JavaScript</option>
+            <option value='Node.js'>Node.js</option>
+            <option value='Express.js'>Express.js</option>
+            <option value='EJS'>EJS</option>
+            <option value='MongoDB'>MongoDB</option>
+            <option value='Bootstrap'>Bootstrap</option>
+            <option value='SASS'>SASS</option>
+            <option value='React'>React</option>
+            <option value='Redux'>Redux</option>
+            <option value='Svelte'>Svelte</option>
+            <option value='Sapper'>Sapper</option>
+            <option value='Socket.IO'>Socket.IO</option>
+            <option value='Webpack'>Webpack</option>
+            <option value='TypeScript'>TypeScript</option>
+          </select>
+        </label>
+      </animated.div>
+      <div className='projects-container'>
+        {trail.map(({ x, height, ...rest }, index) => (
+          <animated.div
+            className='project'
+            key={projects[index].name}
+            style={{
+              ...rest,
+              transform: x.interpolate(x => `translate3d(0, ${x}px, 0)`)
+            }}
+          >
+            <h3 className='project__title'>{projects[index].name}</h3>
+            <picture className='project__picture'>
+              <source srcSet={`${projects[index].imgs[3]}`} media="(max-width: 200px)" type='image/webp'/>
+              <source srcSet={`${projects[index].imgs[4]}`} media="(min-width: 201px) and (max-width: 400px)" type='image/webp'/>
+              <source srcSet={`${projects[index].imgs[5]}`} media="(min-width: 401px)" type='image/webp'/>
+              <source srcSet={`${projects[index].imgs[0]}`} media="(max-width: 200px)" />
+              <source srcSet={`${projects[index].imgs[1]}`} media="(min-width: 201px) and (max-width: 400px)" />
+              <source srcSet={`${projects[index].imgs[2]}`} media="(min-width: 401px)" />
+              <img
+                className='project__img'
+                src={projects[index].imgs[0]}
+                alt={`${projects[index].name} screenshot`}
+              />
+            </picture>
+            <p className='project__description'>
+              {projects[index].description}
+            </p>
+            <p className='project__technologies'>
+              <span>Built with: </span>
+              {projects[index].technologies.join(', ')}.
+            </p>
+            <div className='project__links'>
+              <a className='project__link' href={projects[index].demoUrl}>
+                DEMO
+              </a>
+              <a className='project__link' href={projects[index].codeUrl}>
+                CODE
+              </a>
+            </div>
+            <nav className='project__nav'>
+              <BackToTopButton />
+            </nav>
+          </animated.div>
+        ))}
+      </div>
+    </section>
   );
 };
--- src/components/ProjectsPage/projects.js
+++ src/components/ProjectsPage/projects.js
@@ -1,12 +1,21 @@
 import vinylS from '../../assets/images/vinyl-store-200.png';
 import vinylM from '../../assets/images/vinyl-store-400.png';
 import vinylL from '../../assets/images/vinyl-store-800.png';
+import vinylSw from '../../assets/images/vinyl-store-200.webp';
+import vinylMw from '../../assets/images/vinyl-store-400.webp';
+import vinylLw from '../../assets/images/vinyl-store-800.webp';
 import gibberS from '../../assets/images/gibber-200.png';
 import gibberM from '../../assets/images/gibber-400.png';
 import gibberL from '../../assets/images/gibber-800.png';
+import gibberSw from '../../assets/images/gibber-200.webp';
+import gibberMw from '../../assets/images/gibber-400.webp';
+import gibberLw from '../../assets/images/gibber-800.webp';
 import tempomatS from '../../assets/images/tempomat-200.png';
 import tempomatM from '../../assets/images/tempomat-400.png';
 import tempomatL from '../../assets/images/tempomat-800.png';
+import tempomatSw from '../../assets/images/tempomat-200.webp';
+import tempomatMw from '../../assets/images/tempomat-400.webp';
+import tempomatLw from '../../assets/images/tempomat-800.webp';
 import simple808S from '../../assets/images/simple808-200.png';
 import simple808M from '../../assets/images/simple808-400.png';
 import simple808L from '../../assets/images/simple808-800.png';
@@ -25,12 +34,30 @@ import caheeL from '../../assets/images/cahee-800.png';
 import wtwS from '../../assets/images/wtw-200.png';
 import wtwM from '../../assets/images/wtw-400.png';
 import wtwL from '../../assets/images/wtw-800.png';
+import simple808Sw from '../../assets/images/simple808-200.webp';
+import simple808Mw from '../../assets/images/simple808-400.webp';
+import simple808Lw from '../../assets/images/simple808-800.webp';
+import weatherSw from '../../assets/images/weather-200.webp';
+import weatherMw from '../../assets/images/weather-400.webp';
+import weatherLw from '../../assets/images/weather-800.webp';
+import portfolioSw from '../../assets/images/portfolio-200.webp';
+import portfolioMw from '../../assets/images/portfolio-400.webp';
+import portfolioLw from '../../assets/images/portfolio-800.webp';
+import productlySw from '../../assets/images/productly-200.webp';
+import productlyMw from '../../assets/images/productly-400.webp';
+import productlyLw from '../../assets/images/productly-800.webp';
+import caheeSw from '../../assets/images/cahee-200.webp';
+import caheeMw from '../../assets/images/cahee-400.webp';
+import caheeLw from '../../assets/images/cahee-800.webp';
+import wtwSw from '../../assets/images/wtw-200.webp';
+import wtwMw from '../../assets/images/wtw-400.webp';
+import wtwLw from '../../assets/images/wtw-800.webp';
 
 const projects = [
   {
     name: 'What\'s the Weather v2',
     description: 'Remake of my weather APP. User can check actual weather and forecasts for current or chosen location. App settings allow to choose 2 languages: polish and english; and 2 modes: light and dark. It\'s my first project with TypeScript. Project contains server which formats data incoming from external APIs.',
-    imgs: [ wtwS, wtwM, wtwL ],
+    imgs: [ wtwS, wtwM, wtwL, wtwSw, wtwMw, wtwLw ],
     technologies: ['React', 'SASS', 'JavaScript', 'CSS3', 'HTML5', 'TypeScript', 'Node.js', 'Express.js'],
     demoUrl: 'https://bohdanone.github.io/whats-the-weather-v2/',
     codeUrl: 'https://github.com/BohdanOne/whats-the-weather-v2'
@@ -38,7 +65,7 @@ const projects = [
   {
     name: 'Portfolio Page',
     description: 'My personal developer portfolio page. In the project I try to explore and learn modern features of React (e.g Hooks) and Redux. I also make use of react-related libraries: react-router, react-spring. App includes my first experiments with Web Audio API. Page still under development - more features on the way!',
-    imgs: [ portfolioS, portfolioM, portfolioL ],
+    imgs: [ portfolioS, portfolioM, portfolioL, portfolioSw, portfolioMw, portfolioLw ],
     technologies: ['React', 'Redux', 'SASS', 'JavaScript', 'CSS3', 'HTML5'],
     demoUrl: 'https://bohdanone.github.io/',
     codeUrl: 'https://github.com/BohdanOne/bohdanone.github.io'
@@ -46,7 +73,7 @@ const projects = [
   {
     name: 'Vinyl Store',
     description: 'Application for all music lovers. Guest user can browse collection of record stores (each entry has info, rating, location on google maps, opening hours and reviews). Registered user can edit or remove own profile; see other users profiles; add, edit and delete stores; add, edit and delete reviews. When building this app, my goal was to make full-stack app with implementation of REST API. Demo hosted on Heroku - might take few seconds to load!',
-    imgs: [ vinylS, vinylM, vinylL ],
+    imgs: [ vinylS, vinylM, vinylL, vinylSw, vinylMw, vinylLw ],
     technologies: ['MongoDB', 'Express.js', 'Node.js', 'EJS', 'Bootstrap', 'JavaScript', 'CSS3', 'HTML5'],
     demoUrl: 'https://vinyl-store.herokuapp.com/',
     codeUrl: 'https://github.com/BohdanOne/vinyl-store'
@@ -54,7 +81,7 @@ const projects = [
   {
     name: '! gibber',
     description: 'Minimal Chat App. My first experiments with Sapper and with Socket.IO. Demo hosted on Heroku - might take few seconds to load. App still under development - more features on the way!',
-    imgs: [ gibberS, gibberM, gibberL ],
+    imgs: [ gibberS, gibberM, gibberL, gibberSw, gibberMw, gibberLw ],
     technologies: ['Svelte', 'Sapper', 'Socket.IO', 'JavaScript', 'CSS3', 'HTML5'],
     demoUrl: 'https://gibber-chat.herokuapp.com/',
     codeUrl: 'https://github.com/BohdanOne/gibber'
@@ -62,7 +89,7 @@ const projects = [
   {
     name: 'Tempomat',
     description: 'Tool for musicians. Convert BPM (beats-per-minute) to Millisceconds. App also display italian tempo markings name for given BPM. By default it uses quarter note subdivision, but user can also choose other subdivisions. This is my first application written in Svelte.',
-    imgs: [ tempomatS, tempomatM, tempomatL ],
+    imgs: [ tempomatS, tempomatM, tempomatL, tempomatSw, tempomatMw, tempomatLw ],
     technologies: ['Svelte', 'JavaScript', 'CSS3', 'HTML5'],
     demoUrl: 'https://bohdanone.github.io/tempomat',
     codeUrl: 'https://github.com/BohdanOne/tempomat'
@@ -70,7 +97,7 @@ const projects = [
   {
     name: "What's the Weather",
     description: 'User can check actual weather condition or forecasts fetched from AccuWeather API. If user allow geolocation then app will determine his location using browsers Geolocation API. If geolocation is not allowed (or user wants to check weather for other place) there is also text search option. App built to practice connecting to external API and inserting fetched data in the DOM with Vanilla JS. It was also my first experiment with Geolocation API.',
-    imgs: [ weatherS, weatherM, weatherL ],
+    imgs: [ weatherS, weatherM, weatherL, weatherSw, weatherMw, weatherLw ],
     technologies: ['JavaScript', 'CSS3', 'HTML5', 'SASS', 'Webpack'],
     demoUrl: 'https://bohdanone.github.io/whatstheweather',
     codeUrl: 'https://github.com/BohdanOne/whatstheweather'
@@ -78,7 +105,7 @@ const projects = [
   {
     name: 'Simple 808',
     description: 'Drum machine with loaded sound of legendary TR-808. You can play using keyboard or pressing pads on the screen. Built for fun to practice interactions with DOM elements in Vanilla Java Script.',
-    imgs: [ simple808S, simple808M, simple808L ],
+    imgs: [ simple808S, simple808M, simple808L, simple808Sw, simple808Mw, simple808Lw ],
     technologies: ['JavaScript', 'CSS3', 'HTML5'],
     demoUrl: 'https://bohdanone.github.io/simple808',
     codeUrl: 'https://github.com/BohdanOne/simple808'
@@ -86,7 +113,7 @@ const projects = [
   {
     name: 'Productly',
     description: 'Landing Page created in pure HTML and CSS. Based on free design found at dribbble.com.',
-    imgs: [ productlyS, productlyM, productlyL ],
+    imgs: [ productlyS, productlyM, productlyL, productlySw, productlyMw, productlyLw ],
     technologies: ['Webpack', 'SASS', 'CSS3', 'HTML5'],
     demoUrl: 'https://bohdanone.github.io/productly',
     codeUrl: 'https://github.com/BohdanOne/productly'
@@ -94,7 +121,7 @@ const projects = [
   {
     name: 'Cahee',
     description: 'Landing Page created to practice psd to html coding. Based on free design from Weekly Webdev Challenge facebook group.',
-    imgs: [ caheeS, caheeM, caheeL ],
+    imgs: [ caheeS, caheeM, caheeL, caheeSw, caheeMw, caheeLw ],
     technologies: ['Webpack', 'JavaScript', 'SASS', 'CSS3', 'Bootstrap', 'HTML5'],
     demoUrl: 'https://bohdanone.github.io/cahee/',
     codeUrl: 'https://github.com/BohdanOne/cahee'
